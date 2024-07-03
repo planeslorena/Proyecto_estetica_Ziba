bf50deb032469fe9463402b56ee60177d786d550
Guada-Chojo <gchojohernandez@gmail.com>
2024-07-03 19:27:02 -0300

    boton client page a appointment page routeado

--- ziba-react/src/app/components/newAppointment/newAppointment.tsx
+++ ziba-react/src/app/components/newAppointment/newAppointment.tsx
@@ -1,10 +1,12 @@
+import { useRouter } from 'next/navigation';
 import './newAppointment.css';
 
 export const NewAppointment = () => {
+    const router = useRouter();
     return(
         <div className="d-flex justify-content-evenly align-items-center client-appointment-button-container">
             <p className='client-appointment-text'>¿No tiene turno todavía?</p>
-            <button className='client-appointment-button'>Reserve uno ahora</button>
+            <button onClick={() => { router.push('/appointment') }} className='client-appointment-button'>Reserve uno ahora</button>
         </div>
     )
 }
